dcafcc2cba6918fffb13c2568b7becfe738d4283
Aaron Enders <mail@aaron-enders.de>
2017-05-10 22:16:48 +0200

    -

--- Resources/Public/JavaScript/Admin.js
+++ Resources/Public/JavaScript/Admin.js
@@ -38,5 +38,13 @@ if (typeof jQuery == 'undefined') {
 		
 	})
 
+	$(document).on("click", "ul.tabs li",function() {
+		var tab_id = $(this).attr('data-tab');
 
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 } ) ( t3jQuery );
