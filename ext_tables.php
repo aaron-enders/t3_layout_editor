ef1fd554630b208ab91f96999a05cee0a8334544
enders <enders@bluehouse.de>
2018-01-16 10:33:01 +0100

    Enable/Disable backend module

--- ext_tables.php
+++ ext_tables.php
@@ -4,26 +4,23 @@ if (!defined('TYPO3_MODE')) {
 }
 
 if (TYPO3_MODE === 'BE') {
-
-	/**
-	 * Registers a Backend Module
-	 */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'LayoutEditor.' . $_EXTKEY,
-		'tools',	 // Make module a submodule of 'tools'
-		'admin',	// Submodule key
-		'',						// Position
-		array(
-			'Admin' => 'index',
-			
-		),
-		array(
-			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.png',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_admin.xlf',
-		)
-	);
-
+	$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['layout_editor']);
+	if (!isset($configuration['showModule']) || $configuration['showModule'] == '1'){
+		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+			'LayoutEditor.' . $_EXTKEY,
+			'tools',
+			'admin',
+			'',	
+			array(
+				'Admin' => 'index',
+			),
+			array(
+				'access' => 'user,group',
+				'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.png',
+				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_admin.xlf',
+			)
+		);
+	}
 }
 
 $sql = "SELECT uid, name, class, sorting FROM tx_layouteditor_domain_model_layouts_content WHERE deleted='0' AND hidden='0' ORDER BY sorting ASC";
@@ -46,12 +43,5 @@ while ( $out = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($rs)){
 		page.bodyTagCObject.'.$id.'.value = '.$out['class']);
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Layout Editor');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_layouteditor_domain_model_admin', 'EXT:layout_editor/Resources/Private/Language/locallang_csh_tx_layouteditor_domain_model_admin.xlf');
-// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_layouteditor_domain_model_admin');
-
-
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/ts/layoutEditor/PageTS/temp.txt">' );
-
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', ' ');
