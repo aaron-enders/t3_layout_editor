05c11584bda2ed59fa1d792117a887690e372a54
enders <enders@bluehouse.de>
2018-01-18 11:12:53 +0100

    Added powermail layouts

---
+++ Configuration/TCA/powermail.php
@@ -0,0 +1,96 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+$GLOBALS['TCA']['tx_layouteditor_domain_model_layouts_powermail'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:layout_editor/Resources/Private/Language/locallang.xlf:powermailLayouts',
+		'label' => 'name',
+        'label_alt_force' => 1,
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+		'delete' => 'deleted',
+		'sortby' => 'sorting',
+		'rootLevel' => 1,
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+		),
+		'searchFields' => 'name,class',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath("layout_editor") . 'ext_icon.png'
+	),
+	'interface' => array(
+		'showRecordFieldList' => 'name, class, type',
+	),
+	'types' => array(
+		'1' => array('showitem' => 'type, name, class, references'),
+	),
+	'columns' => array(
+		'hidden' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+			),
+		),
+		'name' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:layout_editor/Resources/Private/Language/locallang.xlf:layoutName',
+			'config' => array(
+				'type' => 'input',
+				'size' => 30,
+				'eval' => 'trim'
+			),
+		),
+		'class' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:layout_editor/Resources/Private/Language/locallang.xlf:classPlaceholder',
+			'config' => array(
+				'type' => 'input',
+				'size' => 30,
+				'eval' => 'trim'
+			),
+		),
+		'type' => array (
+	        'exclude' => 0,
+	        'label' => 'Typ',
+	        'config' => array (
+                'type' => 'select',
+                'items' => array (
+                	array('Formular', 'form'),
+                    array('Seite', 'page'),
+                    array('Feld', 'field') 
+                ),
+                'size' => 1,
+                'maxitems' => 1,
+	        )
+		),
+		'references' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:layout_editor/Resources/Private/Language/locallang.xlf:usedInElements',
+			'config' => array(
+				'type' => 'inline',
+				'readOnly' => 1,
+				'foreign_table' => 'tt_content',
+				'foreign_field' => 'layout',
+				'appearance' => [
+			        'enabledControls' => [
+			            'info' => TRUE,
+			            'new' => FALSE,
+			            'dragdrop' => TRUE,
+			            'sort' => FALSE,
+			            'hide' => TRUE,
+			            'delete' => FALSE,
+			            'localize' => FALSE,
+			        ],
+			    ],
+			),
+		),
+	),
+);
+
+
+
+
+
--- ext_tables.php
+++ ext_tables.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 if (TYPO3_MODE === 'BE') {
 	$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['layout_editor']);
-	if (!isset($configuration['showModule']) || $configuration['showModule'] == '1'){
+	if ($configuration['showModule'] == '1'){
 		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
 			'LayoutEditor.' . $_EXTKEY,
 			'tools',
@@ -41,6 +41,11 @@ while ( $out = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($rs)){
 		page.bodyTagCObject.'.$id.' = TEXT
 		page.bodyTagCObject.'.$id.'.wrap = <body class="|">
 		page.bodyTagCObject.'.$id.'.value = '.$out['class']);
+}
+$sql = "SELECT uid, type, name, class, sorting FROM tx_layouteditor_domain_model_layouts_powermail WHERE deleted='0' AND hidden='0' ORDER BY sorting ASC";
+$rs = $GLOBALS['TYPO3_DB']->sql_query($sql);
+while ( $out = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($rs)){ 
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig( 'TCEFORM.tx_powermail_domain_model_'.$out['type'].'.css.addItems.'.$out['class'].' = '.$out['name'] );
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Layout Editor');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_layouteditor_domain_model_admin', 'EXT:layout_editor/Resources/Private/Language/locallang_csh_tx_layouteditor_domain_model_admin.xlf');
