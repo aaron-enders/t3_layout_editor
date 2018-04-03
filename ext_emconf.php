ec07f78a1d4095087fc8121b06dcefeb64012956
Aaron Enders <mail@aaron-enders.de>
2018-04-03 15:56:44 +0200

    T3 8.7.11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '1',
 	'createDirs' => 'fileadmin/ts/layoutEditor/PageTS,fileadmin/ts/layoutEditor/TypoScript',
 	'clearCacheOnLoad' => 1,
-	'version' => '1.3.5',
+	'version' => '1.3.6',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-8.7.9',
+			'typo3' => '6.2.0-8.7.11',
 		),
 		'conflicts' => array(
 		),
